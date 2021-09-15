2fea32088f03dab40125d18c8632ef0f5e17c4c6
Pratiksharaut14 <82270724+Pratiksharaut14@users.noreply.github.com>
2021-09-15 22:43:28 +0530

    Modal javascript added for deleting card

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+let globalTaskData = [];
+taskContents = document.getElementById("taskContentsRow");
+
 const addCard = () => {
   const newTaskDetails = {
     id: `${Date.now()}`,
     url: document.getElementById("imgUrl").value,
     title: document.getElementById("taskTitle").value,
     type: document.getElementById("taskType").value,
-    desc: document.getElementById("taskDesc").value
+    desc: document.getElementById("taskDesc").value,
   };
-  taskContents = document.getElementById("taskContentsRow");
   taskContents.insertAdjacentHTML("beforeend", generateTaskCard(newTaskDetails));
+
+  globalTaskData.push(newTaskDetails);
+  saveToLocalStorage();
 };
 
 const generateTaskCard = ({ id, url, title, type, desc }) => {
@@ -15,11 +20,12 @@ const generateTaskCard = ({ id, url, title, type, desc }) => {
       <div class="card">
           <div class="card-header">
               <div class="card-header d-flex  justify-content-end">
-                  <button type="button" class="btn btn-outline-info ">
+                  <button type="button" class="btn btn-outline-info me-2">
                       <i class="fas fa-pencil-alt"></i>
                   </button>
-                  <button class="btn btn-outline-danger">
-                      <i class="fas fa-trash"></i>
+                  <button class="btn btn-outline-danger" name=${id}onclick="deleteTask(this)">
+                      <i class="fas fa-trash" name=${id}
+                      onclick="deleteTask        (this)"></i>
                   </button>
               </div>
           </div>
@@ -43,4 +49,28 @@ const generateTaskCard = ({ id, url, title, type, desc }) => {
           </div>
       </div>
   </div>`;
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("mytasks", JSON.stringify({ tasks: globalTaskData }));
+};
+
+const reloadTaskCard = () => {
+  const localStorageCopy = JSON.parse(localStorage.getItem("mytasks"));
+  console.log(localStorageCopy);
+  if (localStorageCopy) {
+    globalTaskData = localStorageCopy["tasks"];
+  }
+  console.log(globalTaskData);
+  globalTaskData.map((cardData) => {
+    taskContents.insertAdjacentHTML("beforeend", generateTaskCard(cardData));
+  });
+};
+
+const deleteTask = (e) => {
+  const targetID = e.getAttribute("name");
+  globalTaskData = globalTaskData.filter((cardData) => cardData.id !== targetID);
+  console.log(globalTaskData);
+  saveToLocalStorage();
+  window.location.reload();
 };
